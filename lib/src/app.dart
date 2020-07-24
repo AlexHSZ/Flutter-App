e4ecb6b71b0134349b12f73dd1f1212a1a148049
Alexander Ho <alexhsz98@gmail.com>
2020-07-24 21:07:54 +0100

    Refactiored stateless widget to stateful.

---
+++ lib/src/app.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class App extends StatefulWidget {
+  createState() {
+    return AppState();
+  }
+}
+
+class AppState extends State<App> {
+  int count = 0;
+
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Text('Counter: $count'),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {
+            setState(() {
+              count += 1;
+            });
+          },
+        ),
+        appBar: AppBar(
+          title: Text("Flutter App"),
+        ),
+      ),
+    );
+  }
+}
