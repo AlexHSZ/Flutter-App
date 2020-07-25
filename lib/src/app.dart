ebfa9c86ed979a0bc420ac6297675630d90f9c2f
Alexander Ho <alexhsz98@gmail.com>
2020-07-25 23:51:40 +0100

    Initial creation of image list class.

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' show get;
 import 'models/image_model.dart';
+import 'widgets/image_list.dart';
 import 'dart:convert';
 
 class App extends StatefulWidget {
@@ -11,18 +12,23 @@ class App extends StatefulWidget {
 
 class AppState extends State<App> {
   int count = 0;
+  List<ImageModel> images = [];
 
   void fetchImage() async {
     count++;
     var response =
         await get('https://jsonplaceholder.typicode.com/photos/$counter');
     var imageModel = ImageModel.fromJson(json.decode(response.body));
+
+    setState(() {
+      images.add(imageModel);
+    });
   }
 
   Widget build(context) {
     return MaterialApp(
       home: Scaffold(
-        body: Text('Counter: $count'),
+        body: ImageList(images),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
           onPressed: fetchImage,
