32df10a87ee09add7b6ae24c8953573b871fdebb
Alexander Ho <alexhsz98@gmail.com>
2020-07-25 23:34:13 +0100

    Added async away for http get req

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
+import 'models/image_model.dart';
 import 'dart:convert';
 
 class App extends StatefulWidget {
@@ -10,17 +12,20 @@ class App extends StatefulWidget {
 class AppState extends State<App> {
   int count = 0;
 
+  void fetchImage() async {
+    count++;
+    var response =
+        await get('https://jsonplaceholder.typicode.com/photos/$counter');
+    var imageModel = ImageModel.fromJson(json.decode(response.body));
+  }
+
   Widget build(context) {
     return MaterialApp(
       home: Scaffold(
         body: Text('Counter: $count'),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
-          onPressed: () {
-            setState(() {
-              count += 1;
-            });
-          },
+          onPressed: fetchImage,
         ),
         appBar: AppBar(
           title: Text("Flutter App"),
